41ea6a41e6fec223e31aab2a79ee61f0744ef9a4
lc1995 <498994046@qq.com>
2016-08-17 12:53:07 +0800

    2D Phisics Engine in C#

---
+++ Phisics_2D/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Phisics_2D
+{
+    public partial class Form1 : Form
+    {
+        // 用于测试的单位
+        private PhisicalObject_2D pig = new PhisicalObject_2D(450, 480, 20, 20, "", 0, 0);
+
+        private WallObject_2D wall_0 = new WallObject_2D(100, 400, 50, 20, "");
+        private WallObject_2D wall_1 = new WallObject_2D(200, 400, 50, 20, "");
+        private WallObject_2D wall_2 = new WallObject_2D(300, 400, 50, 20, "");
+        private WallObject_2D wall_3 = new WallObject_2D(400, 400, 50, 20, "");
+        private WallObject_2D wall_4 = new WallObject_2D(150, 300, 50, 20, "");
+        private WallObject_2D wall_5 = new WallObject_2D(250, 300, 50, 20, "");
+        private WallObject_2D wall_6 = new WallObject_2D(350, 300, 50, 20, "");
+        private WallObject_2D wall_7 = new WallObject_2D(200, 200, 50, 20, "");
+        private WallObject_2D wall_8 = new WallObject_2D(300, 200, 50, 20, "");
+        private WallObject_2D wall_9 = new WallObject_2D(250, 100, 50, 20, "");
+
+        // 屏幕外墙，不需要绘制
+        private WallObject_2D wallUpside = new WallObject_2D(-100, -100, 700, 100, "");
+        private WallObject_2D wallDownside = new WallObject_2D(-100, 500, 700, 100, "");
+        private WallObject_2D wallLeftside = new WallObject_2D(-100, 0, 100, 500, "");
+        private WallObject_2D wallRightside = new WallObject_2D(500, 0, 100, 500, "");
+
+        // 数组，用于保存所有非重复键值
+        private ArrayList aryKeys = new ArrayList();
+
+        private GameScene gameScene;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 初始化游戏画面
+            gameScene = new GameScene();
+            gameScene.Location = new Point(10, 10);
+            gameScene.Size = new Size(500, 500);
+            gameScene.BackColor = Color.Black;
+            gameScene.Paint += gameScene_Paint;
+            Controls.Add(gameScene);
+
+        }
+
+        // 物理引擎计时
+        private void phsicalTimer_Tick(object sender, EventArgs e)
+        {
+            // 重力加速度，更新Y方向速度
+            if(pig.HasGravity())
+                pig.YSp += 0.4;
+
+            // 更新位置
+            pig.PreY += pig.YSp;
+            pig.Y = (int)pig.PreY;
+            pig.PreX += pig.XSp;
+            pig.X = (int)pig.PreX;
+
+            gameScene.Invalidate();
+
+            // 碰撞检测并回滚位置
+            WallObject_2D w = pig.IsCrashedWithWall();
+            bool isCrash = false;
+            while (pig.IsCrashedWithWall() != null)
+            {
+                // 放慢100倍，往回补偿，精度为1像素
+                pig.PreY -= pig.YSp / 100;
+                pig.PreX -= pig.XSp / 100;
+                pig.Y = (int)pig.PreY;
+                pig.X = (int)pig.PreX;
+
+                // 发生碰撞
+                isCrash = true;
+            }
+
+            // 碰撞方向检测
+            if(isCrash && w != null)
+            {
+                // 右侧碰撞
+                if((pig.X + pig.Width) <= w.X)
+                {
+                    pig.XSp = 0;
+                }
+                // 左侧碰撞
+                else if(pig.X >= (w.X + w.Width))
+                {
+                    pig.XSp = 0;
+                }
+                // 底部碰撞
+                else if((pig.Y + pig.Height) <= w.Y)
+                {
+                    pig.YSp = 0;
+                }
+                // 顶部碰撞
+                else if(pig.Y >= (w.Y + w.Height))
+                {
+                    pig.YSp = 0;
+                }
+            }
+        }
+
+        // 自定义游戏画面控件，双缓冲
+        class GameScene : PictureBox
+        {
+            public GameScene() : base()
+            {
+                this.DoubleBuffered = true;
+                this.SetStyle(ControlStyles.DoubleBuffer | ControlStyles.UserPaint |
+                                           ControlStyles.AllPaintingInWmPaint,
+                                           true);
+
+                this.UpdateStyles();
+            }
+        }
+
+        // 游戏画面重绘
+        private void gameScene_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            Rectangle rect = new Rectangle(pig.X, pig.Y, pig.Width, pig.Height);
+            SolidBrush brush = new SolidBrush(Color.Red);
+
+            g.FillRectangle(brush, rect);
+
+            SolidBrush brush2 = new SolidBrush(Color.Yellow);
+            foreach(WallObject_2D w in WallObject_2D.aryWall2D)
+            {
+                rect = new Rectangle(w.X, w.Y, w.Width, w.Height);
+                g.FillRectangle(brush2, rect);
+            }
+        }
+
+        // 按键响应
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (aryKeys.Contains(e.KeyCode))
+                return;
+
+            aryKeys.Add(e.KeyCode);
+            switch(e.KeyCode)
+            {
+                case Keys.W:
+                    pig.YSp = -10;
+                    break;
+                case Keys.A:
+                    pig.XSp = -5;
+                    break;
+                case Keys.D:
+                    pig.XSp = 5;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            aryKeys.Remove(e.KeyCode);
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    break;
+                case Keys.A:
+                    pig.XSp = 0;
+                    break;
+                case Keys.D:
+                    pig.XSp = 0;
+                    break;
+            }
+        }
+    }
+}
---
+++ Phisics_2D/PhisicalObject_2D.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Phisics_2D
+{
+    // 2D对象类
+    abstract class _2DObject : object
+    {
+        // 属性
+        // 精确横坐标，更新实际横坐标
+        public double PreX { set; get; }
+        // 精确纵坐标，更新实际纵坐标
+        public double PreY { set; get; }
+        // 实际横坐标
+        public int X { set; get; }
+        // 实际纵坐标
+        public int Y { set; get; }
+        // 宽度
+        public int Width { set; get; }
+        // 高度
+        public int Height { set; get; }
+
+        // 贴图文件
+        public string TextureFile { set; get; }
+
+        // 方法
+        // 构造函数
+        public _2DObject(double x = 0, double y = 0, int width = 0, int height = 0, string textureFile = "")
+        {
+            PreX = x;
+            PreY = y;
+            X = (int)PreX;
+            Y = (int)PreY;
+            Width = width;
+            Height = height;
+            TextureFile = textureFile;
+        }
+
+        // 是否超出边界
+        public bool IsOutOfRange(int XRange, int YRange)
+        {
+            if (X >= 0 && Y >= 0 && X <= (XRange - Height) && Y <= (YRange - Height))
+                return false;
+            else
+                return true;
+        }
+        // 是否碰撞
+        public bool IsCrashed(_2DObject obj)
+        {
+            System.Drawing.Rectangle rect1 = new System.Drawing.Rectangle(X, Y, Height, Height);
+            System.Drawing.Rectangle rect2 = new System.Drawing.Rectangle(obj.X, obj.Y, obj.Height, obj.Height);
+            if (rect1.IntersectsWith(rect2))
+                return true;
+            else
+                return false;
+        }
+    }
+
+    class PhisicalObject_2D : _2DObject
+    {
+        // X，Y方向速度，最大值为100
+        private double xSp;
+        private double ySp;
+        public double XSp
+        {
+            get
+            {
+                return xSp;
+            }
+            set
+            {
+                if (value <= 100 && value >= -100)
+                    xSp = value;
+                else if (value > 100)
+                    xSp = 100;
+                else
+                    xSp = -100;
+            }
+        }
+        public double YSp
+        {
+            get
+            {
+                return ySp;
+            }
+            set
+            {
+                if (value <= 100 && value >= -100)
+                    ySp = value;
+                else if (value > 100)
+                    ySp = 100;
+                else
+                    ySp = -100;
+            }
+        }
+
+        // 静态数组，存放所有2D物理对象
+        public static List<PhisicalObject_2D> aryPhic2D = new List<PhisicalObject_2D>();
+
+        // 构造函数
+        public PhisicalObject_2D(double x = 0, double y = 0, int width = 0, int height = 0, string textureFile = "",
+            double x_Sp = 0, double y_Sp = 0) : base(x, y, width, height, textureFile)
+        {
+            XSp = x_Sp;
+            YSp = y_Sp;
+
+            aryPhic2D.Add(this);
+        }
+
+        // 是否与墙体碰撞，如果是，返回墙体
+        public WallObject_2D IsCrashedWithWall()
+        {
+            Rectangle rect1 = new Rectangle(X, Y, Width, Height);
+            Rectangle rect2;
+
+            foreach (WallObject_2D w in WallObject_2D.aryWall2D)
+            {
+                rect2 = new Rectangle(w.X, w.Y, w.Width, w.Height);
+                if (rect1.IntersectsWith(rect2))
+                    return w;
+            }
+
+            return null;
+        }
+
+        // 是否有重力加速度
+        public bool HasGravity()
+        {
+            foreach(WallObject_2D w in WallObject_2D.aryWall2D)
+            {
+                if (Y == (w.Y - Height))
+                {
+                    if ((X < w.X + w.Width) && (X + Width > w.X))
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    class WallObject_2D : _2DObject
+    {
+        // 静态数组，存放所有2D墙体对象
+        public static List<WallObject_2D> aryWall2D = new List<WallObject_2D>();
+
+        // 构造函数
+        public WallObject_2D(double x = 0, double y = 0, int width = 0, int height = 0, string textureFile = "") : base(x, y, width, height, textureFile)
+        {
+            aryWall2D.Add(this);
+        }
+    }
+}
